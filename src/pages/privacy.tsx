6f9e0a8f55f9918ab9e040cf06d548b0897e0233
Adam Saladino <asaladino@users.noreply.github.com>
2024-03-20 18:57:11 -0400

    Create privacy.tsx

---
+++ src/pages/privacy.tsx
@@ -0,0 +1,17 @@
+import * as React from 'react'
+
+import DefaultLayout from '../components/DefaultLayout'
+import FractureTitle from '../components/FractureTitle'
+
+const PageTwo = () => (
+  <DefaultLayout>
+    <h2>
+      <FractureTitle split="word">Privacy Policy</FractureTitle>
+    </h2>
+    <div className="content" style={{ clear: 'both' }}>
+      <p>Coding Simply does NOT collect any user data or distribute user data. Done.</p>
+    </div>
+  </DefaultLayout>
+)
+
+export default PageTwo
